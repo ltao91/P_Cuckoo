1cd1977172fb0359297cd6327623c90208ea52a5
wawawa113 <ryo241334@icloud.com>
2022-12-23 14:03:59 +0900

    fix timestamp

--- bench/main.cpp
+++ bench/main.cpp
@@ -11,31 +11,37 @@ int main()
     }
     for (int t_num = 1; t_num < 200; t_num *= 2)
     {
-        cout << "start" << endl;
-        OptCuckoo<int> Cuckoo(8000 * 1000);
-        vector<thread> threads;
-        auto put = [](int tid, vector<pair<string, int>> &args, OptCuckoo<int> &Cuckoo, int t_num)
-        {
-            for (int i = tid; i < ops; i += t_num)
+        double ms_sum=0;
+        double ms_best=1000000000;
+        for(int loop=0;loop<10;loop++){
+            cout << "start" << endl;
+            OptCuckoo<int> Cuckoo(8000 * 1000);
+            vector<thread> threads;
+            auto put = [](int tid, vector<pair<string, int>> &args, OptCuckoo<int> &Cuckoo, int t_num)
             {
-                Cuckoo.put(args[i].first, args[i].second, tid + 1);
-            }
-        };
+                for (int i = tid; i < ops; i += t_num)
+                {
+                    Cuckoo.put(args[i].first, args[i].second, tid + 1);
+                }
+            };
 
-        auto s = get_now();
-        for (int i = 0; i < t_num; i++)
-        {
-            threads.emplace_back(put, i, ref(args), ref(Cuckoo), t_num);
+            auto s = get_now();
+            for (int i = 0; i < t_num; i++)
+            {
+                threads.emplace_back(put, i, ref(args), ref(Cuckoo), t_num);
+            }
+            for (int i = 0; i < t_num; i++)
+            {
+                threads[i].join();
+            }
+            int abortsum = 0;
+            for (auto i : abort_tid)
+                abortsum += i;
+            auto e = get_now();
+            ms_sum+=get_duration_ms(s, e) / 1000000;
+            ms_best=min(ms_best,get_duration_ms(s, e) / 1000000);
         }
-        for (int i = 0; i < t_num; i++)
-        {
-            threads[i].join();
-        }
-        int abortsum=0;
-        for(auto i:abort_tid)abortsum+=i;
-        auto e = get_now();
-        cout <<  get_duration_ms(s, e) / 1000000 << endl;
-        cout << abortsum  << endl;
+        cout<<ms_sum/10<<endl;
+        cout<<ms_best<<endl;
     }
-    
 }
