70c76a634967c9949b23cf13970c1894b8238113
wawawa113 <ryo241334@icloud.com>
2023-01-27 17:10:59 +0900

    simple test

--- bench/main.cpp
+++ bench/main.cpp
@@ -7,75 +7,115 @@
 
 using namespace std;
 
-class HOGE{
-	int n;
-  public:
-	  HOGE():n(10){
-	  }
+class HOGE
+{
+  int n;
 
+public:
+  HOGE() : n(10)
+  {
+  }
 };
 
 int main()
 {
-  const int ops = 16000 * 10000;
-  vector<pair<string, int>> args(ops/100000);
-  //for (int i = 0; i < ops; i++)
-  //{
-  //  args[i].first = "random:temp" + to_string(i);
-  //  args[i].second = i;
-  //}
-  //auto rargs=args;
-  //std::random_device seed_gen;
-  //std::mt19937 engine(seed_gen());
-  //std::shuffle(rargs.begin(),rargs.end(),engine);
-  for (int t_num = 1; t_num < 300; t_num *= 2)
+  // const int ops = 16000 * 10000;
+  // vector<pair<string, int>> args(ops/100000);
+  // //for (int i = 0; i < ops; i++)
+  // //{
+  // //  args[i].first = "random:temp" + to_string(i);
+  // //  args[i].second = i;
+  // //}
+  // //auto rargs=args;
+  // //std::random_device seed_gen;
+  // //std::mt19937 engine(seed_gen());
+  // //std::shuffle(rargs.begin(),rargs.end(),engine);
+  // for (int t_num = 1; t_num < 300; t_num *= 2)
+  // {
+  //   double ms_sum = 0;
+  //   double ms_best = 1000000000;
+  //   for (int loop = 0; loop < 10; loop++)
+  //   {
+  //     //std::cout<<"start:"<<loop<<std::endl;
+  //     //OptCuckoo<int> Cuckoo(5000 * 1000);
+  //     OptCuckoo<int> Cuckoo(10);
+  //     vector<thread> threads;
+  //     auto write = [](bool &flag,int tid, vector<pair<string, int>> &args, OptCuckoo<int> &Cuckoo, int t_num)
+  //     {
+  //       while(!flag){
+  //       }
+  //       for (int i = tid; i < ops; i += t_num)
+  //       {
+  //         //Cuckoo.put(args[i].first, args[i].second, i + 1);
+  //         auto p=new HOGE();
+  // }
+  //     };
+  //     auto read = [](bool &flag,int tid,vector<pair<string,int>> &args,OptCuckoo<int> &Cuckoo, int t_num)
+  //     {
+  //       while(!flag){
+  //       }
+  //       for(int i=tid;i<ops;i+=t_num){
+  //   Cuckoo.get(args[i].first);
+  // }
+  //     };
+
+  //     bool f=false;
+  //     for (int i = 0; i < t_num; i++)
+  //     {
+  //       threads.emplace_back(write, ref(f),i, ref(args), ref(Cuckoo), t_num);
+  //     }
+  //     //for (int i = 0; i < t_num; i++)
+  //     //{
+  //     //  threads.emplace_back(read, ref(f),i, ref(args), ref(Cuckoo), t_num);
+  //     //}
+  //     auto s=get_now();
+  //     f=true;
+  //     for (int i = 0; i < t_num; i++)
+  //     {
+  //       threads[i].join();
+  //     }
+  //     auto e = get_now();
+  //     ms_sum += get_duration_ms(s, e) / 1000000;
+  //     ms_best = min(ms_best, get_duration_ms(s, e) / 1000000);
+  //   }
+  //   cout << ms_best << endl;
+  // }
+
+  int ops = 160000 * 1000;
+  bool flag;
+
+  auto f = [](int n, bool &flag)
   {
-    double ms_sum = 0;
-    double ms_best = 1000000000;
+    while (!flag)
+    {
+    }
+    for(int i=0;i<n;i++){
+      auto p=new HOGE();
+      delete p;
+    }
+  };
+  for (int t = 8; t < 300; t *= 2)
+  {
+    double best = 10000000;
     for (int loop = 0; loop < 10; loop++)
     {
-	    //std::cout<<"start:"<<loop<<std::endl;
-      //OptCuckoo<int> Cuckoo(5000 * 1000);
-      OptCuckoo<int> Cuckoo(10);
-	    vector<thread> threads;
-      auto write = [](bool &flag,int tid, vector<pair<string, int>> &args, OptCuckoo<int> &Cuckoo, int t_num)
+      // std::cout << "start : " << loop << std::endl;
+      flag = false;
+      std::vector<std::thread> threads;
+      for (int i = 0; i < t; i++)
       {
-	      while(!flag){
-	      }
-        for (int i = tid; i < ops; i += t_num)
-        {
-          //Cuckoo.put(args[i].first, args[i].second, i + 1);
-          auto p=new HOGE();
-	}
-      };
-      auto read = [](bool &flag,int tid,vector<pair<string,int>> &args,OptCuckoo<int> &Cuckoo, int t_num)
-      {
-	      while(!flag){
-	      }
-        for(int i=tid;i<ops;i+=t_num){
-	  Cuckoo.get(args[i].first);
-	}
-      };
-
-      bool f=false;
-      for (int i = 0; i < t_num; i++)
-      {
-        threads.emplace_back(write, ref(f),i, ref(args), ref(Cuckoo), t_num);
+        threads.emplace_back(f, ops / t, std::ref(flag));
       }
-      //for (int i = 0; i < t_num; i++)
-      //{
-      //  threads.emplace_back(read, ref(f),i, ref(args), ref(Cuckoo), t_num);
-      //}
-      auto s=get_now();
-      f=true;
-      for (int i = 0; i < t_num; i++)
+      auto s = get_now();
+      flag = true;
+      for (int i = 0; i < t; i++)
       {
         threads[i].join();
       }
       auto e = get_now();
-      ms_sum += get_duration_ms(s, e) / 1000000;
-      ms_best = min(ms_best, get_duration_ms(s, e) / 1000000);
+      // std::cout << get_duration_ms(s, e) << std::endl;
+      best = std::min(best, get_duration_ms(s, e));
     }
-    cout << ms_best << endl;
+    std::cout <<  best << std::endl;
   }
 }
